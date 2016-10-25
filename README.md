992ec39035ebfccba85239ec80427fef8d8c6426
whanrott <eart0208@nexus.ox.ac.uk>
2016-10-25 11:16:24 +0100

    added files and updated README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # R_us_gun_deaths
+
+A quick visualisation of all male firearm deaths and male suicide deaths.
+
+Source Centers for Disease Control: Fatal Injury Reports, National and Regional, 1999 - 2014
+
+http://webappa.cdc.gov/sasweb/ncipc/mortrate10_us.html
---
+++ gun_deaths.R
@@ -0,0 +1,23 @@
+library(ggplot2)
+
+gd <- read.csv("all_us_firearm_deaths_male.csv", stringsAsFactors = F)
+gd <- gd[is.na(gd$Year)==F,]
+gd <- gd[gd$Race == "Black" | gd$Race == "White", ]
+gd$Population <- as.numeric(gd$Population)
+gd$Crude.Rate <- as.numeric(gd$Crude.Rate)
+print(
+  ggplot(gd, aes(Year, Crude.Rate, colour = Age.Group)) + geom_line() + facet_wrap(facets = ~ Race) + xlab("Year") + ylab("Crude Rate of death") + ggtitle("All US Male Firearm Deaths by Age")
+)
+ggsave("gun_deaths.png")
+
+gd_suicide <- read.csv("suicide_firearm_deaths_male.csv", stringsAsFactors = F)
+gd_suicide <- gd_suicide[is.na(gd_suicide$Year)==F,]
+gd_suicide <- gd_suicide[gd_suicide$Race == "Black" | gd_suicide$Race == "White", ]
+gd_suicide$Population <- as.numeric(gd_suicide$Population)
+gd_suicide$Crude.Rate <- as.numeric(gd_suicide$Crude.Rate)
+
+print(
+  ggplot(gd_suicide, aes(Year, Crude.Rate, colour = Age.Group)) + geom_line() + facet_wrap(facets = ~ Race) + xlab("Year") + ylab("Crude Rate of death") + ggtitle("US Male Suicides using a Firearm by Age")
+)
+
+ggsave("suicide_deaths.png")
